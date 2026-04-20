4ec6de127e38baa6ec48c8f79c99a727a1fec651
introtoxicXXl <minhajulabedin648@gmail.com>
2026-04-20 14:34:22 +0600

    questions model done

---
+++ backend/model/questions.model.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
+
+const questionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    jobPosition: {
+        type: String,
+        required: true
+    },
+    jobDescription: {
+        type: String,
+        required: true
+    },
+    jobExperience: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Question", questionSchema);
